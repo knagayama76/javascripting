561d15ce8153bd6e1773e9f60bc6a18d2493af79
Kaoru Nagayama <kaoair@hotmail.com>
2021-04-26 14:18:11 -0400

    javascripting exercise 2

---
+++ function-arguments.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function math(num1, num2, num3) {
+  return num1 + num2 * num3;
+}
+
+console.log(math(53, 61, 67));
---
+++ object-keys.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const myCar = {
+  make: "Honda",
+  model: "Civic",
+  year: "2017",
+};
+
+const keys = Object.keys(myCar);
+console.log(keys);
